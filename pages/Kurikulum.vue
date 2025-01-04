35e24407f42d1189af9c4ba019a074e3f58c0d2b
Rdhii <142205085+Ardiansyah05@users.noreply.github.com>
2025-01-04 16:00:10 +0700

    Add files via upload

--- pages/Kurikulum.vue
+++ pages/Kurikulum.vue
@@ -3,7 +3,7 @@
         <div class="card-header">TJKT</div>
             <div class="card-body">
                 <h5 class="card-title">Teknik Komputer Dan Jaringan</h5>
-            <p class="card-text">Teknik Komputer dan Jaringan merupakan ilmu berbasis Teknologi Informasi dan Komunikasi terkait kemampuan algoritma, dan pemrograman komputer, perakitan komputer, perakitan jaringan komputer, dan pengoperasian perangkat lunak, dan internet. Teknik komputer, dan jaringan juga membutuhkan pemahaman di bidang teknik listrik, dan ilmu komputer sehingga mampu mengembangkan, dan mengintegrasikan perangkat lunak, dan perangkat keras.</p>
+            <p class="card-text text-start">Teknik Komputer dan Jaringan merupakan ilmu berbasis Teknologi Informasi dan Komunikasi terkait kemampuan algoritma, dan pemrograman komputer, perakitan komputer, perakitan jaringan komputer, dan pengoperasian perangkat lunak, dan internet. Teknik komputer, dan jaringan juga membutuhkan pemahaman di bidang teknik listrik, dan ilmu komputer sehingga mampu mengembangkan, dan mengintegrasikan perangkat lunak, dan perangkat keras.</p>
         </div>
     </div>
 
@@ -11,7 +11,7 @@
         <div class="card-header">PPLG</div>
             <div class="card-body">
                 <h5 class="card-title">Pemrograman Perangkat Lunak Dan Gim</h5>
-            <p class="card-text">Rekayasa Perangkat Lunak (RPL, atau dalam bahasa Inggris: Software Engineering atau SE) adalah satu bidang profesi yang mendalami cara-cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas.
+            <p class="card-text text-start">Rekayasa Perangkat Lunak (RPL, atau dalam bahasa Inggris: Software Engineering atau SE) adalah satu bidang profesi yang mendalami cara-cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas.
 
 IEEE Computer Society mendefinisikan rekayasa perangkat lunak sebagai penerapan suatu pendekatan yang sistematis, disiplin dan terkuantifikasi atas pengembangan, penggunaan dan pemeliharaan perangkat lunak, serta studi atas pendekatan-pendekatan ini, yaitu penerapan pendekatan engineering atas perangkat lunak.[1]
 
@@ -23,7 +23,7 @@ Rekayasa perangkat lunak adalah pengubahan perangkat lunak itu sendiri guna meng
         <div class="card-header">TBSM</div>
             <div class="card-body">
                 <h5 class="card-title">Teknik Dan Bisnis Sepeda Motor</h5>
-            <p class="card-text">Teknik dan Bisnis Sepeda Motor adalah kompetensi keahlian pada Bidang Studi Keahlian Teknologi dan Rekayasa Program Studi Keahlian Teknik Otomotif yang menekankan pada keterampilan pelayanan jasa mekanik kendaraan sepeda motor roda dua. Kompetensi Keahlian Teknik dan Bisnis Sepeda Motor menyiapkan peserta didik untuk bekerja pada bidang pekerjaan yang dikelola oleh badan, instansi atau perusahaan maupun pribadi (wirausaha).</p>
+            <p class="card-text text-start">Teknik dan Bisnis Sepeda Motor adalah kompetensi keahlian pada Bidang Studi Keahlian Teknologi dan Rekayasa Program Studi Keahlian Teknik Otomotif yang menekankan pada keterampilan pelayanan jasa mekanik kendaraan sepeda motor roda dua. Kompetensi Keahlian Teknik dan Bisnis Sepeda Motor menyiapkan peserta didik untuk bekerja pada bidang pekerjaan yang dikelola oleh badan, instansi atau perusahaan maupun pribadi (wirausaha).</p>
         </div>
     </div>
 
@@ -31,7 +31,7 @@ Rekayasa perangkat lunak adalah pengubahan perangkat lunak itu sendiri guna meng
         <div class="card-header">DKV</div>
             <div class="card-body">
                 <h5 class="card-title">Desain Komunikasi Visual</h5>
-            <p class="card-text">Jurusan Desain Komunikasi Visual (DKV) adalah studi yang mempelajari ilmu tentang penyampaian pesan dengan menggunakan elemen desain grafis, seperti huruf, warna, gambar, dan lay out. Dalam keilmuan DKV, hal yang dipelajari adalah cara mengolah pesan visual yang efektif secara informatif, komunikatif, dan efektif, yang dikemas sekreatif mungkin sehingga pesan dapat tersampaikan dengan tepat sasaran.</p>
+            <p class="card-text text-start">Jurusan Desain Komunikasi Visual (DKV) adalah studi yang mempelajari ilmu tentang penyampaian pesan dengan menggunakan elemen desain grafis, seperti huruf, warna, gambar, dan lay out. Dalam keilmuan DKV, hal yang dipelajari adalah cara mengolah pesan visual yang efektif secara informatif, komunikatif, dan efektif, yang dikemas sekreatif mungkin sehingga pesan dapat tersampaikan dengan tepat sasaran.</p>
         </div>
     </div>
 
@@ -39,7 +39,7 @@ Rekayasa perangkat lunak adalah pengubahan perangkat lunak itu sendiri guna meng
         <div class="card-header">TOI</div>
             <div class="card-body">
                 <h5 class="card-title">Teknik Otomasi Industri</h5>
-            <p class="card-text">Teknik otomasi industri (TOI) adalah suatu jurusan teknik yang menerapkan sistem pengajaran yang mengarah ke bidang perakitan sistem-sistem di dunia industri yang berbasis otomatis/automatic. Kompetensi ini berdasarkan SK Dirjen 2018 ditempuh dalam 4 tahun masa belajar dan termasuk dalam rumpun bidang keahlian Teknik Ketenagalistrikan.</p>
+            <p class="card-text text-start">Teknik otomasi industri (TOI) adalah suatu jurusan teknik yang menerapkan sistem pengajaran yang mengarah ke bidang perakitan sistem-sistem di dunia industri yang berbasis otomatis/automatic. Kompetensi ini berdasarkan SK Dirjen 2018 ditempuh dalam 4 tahun masa belajar dan termasuk dalam rumpun bidang keahlian Teknik Ketenagalistrikan.</p>
         </div>
     </div>
 </template>
